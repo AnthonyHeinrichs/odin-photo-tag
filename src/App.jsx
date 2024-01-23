5cc610f6ec8ce8379c36de488bb39b28a07e35cb
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2024-01-23 16:40:17 +0100

    Add new game cards to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import "./App.scss"
+import "./App.scss";
+import nintendoCard from "/nintendo64-card.png";
+import prehistoriaCard from "/prehistoria-card.png";
+import dragonIslandCard from "/dragon-island-card.png";
 
 function App() {
 
   return (
     <>
-      <h1  className="title">Games</h1>
-      <div className="">
-        <h2>
-          Game One
-        </h2>
-        <h2>
-          Game Two
-        </h2>
-        <h2>
-          Game Three
-        </h2>
+      <h1  className="title">Choose a Game</h1>
+      <div className="games">
+        <div className="game">
+          <img className="game__image" src={nintendoCard} alt="nintendo-64"/>
+          <h4>Nintendo 64</h4>
+          <p>Easy</p>
+        </div>
+        <div className="game">
+          <img className="game__image" src={prehistoriaCard} alt="prehistoria"/>
+          <h4>Prehistoria</h4>
+          <p>Medium</p>
+        </div>
+        <div className="game">
+          <img className="game__image" src={dragonIslandCard} alt="dragon-island"/>
+          <h4>Dragon Charmer's Island</h4>
+          <p>Hard</p>
+        </div>
       </div>
     </>
   )
