90ed57648ef8584336a80054d6727ec6d8cbff5d
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2024-01-23 18:21:11 +0100

    Add dark and light theme for app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "./components/ThemeContext";
 import "./App.scss";
 import Header from "./components/Header";
 import nintendoCard from "/nintendo64-card.png";
@@ -5,31 +8,34 @@ import prehistoriaCard from "/prehistoria-card.png";
 import dragonIslandCard from "/dragon-island-card.png";
 
 function App() {
+  const { theme } = useTheme();
 
   return (
     <>
-      <Header />
-      <main>
+      <Header theme={theme}/>
+      <main className={`main main__${theme}`}>
         <h1  className="title">Choose a Game</h1>
         <div className="games">
-          <div className="game">
+          <div className={`game game__${theme}`}>
             <img className="game__image" src={nintendoCard} alt="nintendo-64"/>
             <h4>Nintendo 64</h4>
             <p>Easy</p>
           </div>
-          <div className="game">
+          <div className={`game game__${theme}`}>
             <img className="game__image" src={prehistoriaCard} alt="prehistoria"/>
             <h4>Prehistoria</h4>
             <p>Medium</p>
           </div>
-          <div className="game">
+          <div className={`game game__${theme}`}>
             <img className="game__image" src={dragonIslandCard} alt="dragon-island"/>
             <h4>Dragon Charmer's Island</h4>
             <p>Hard</p>
           </div>
         </div>
         <div className="leaderboard">
-          <h4>View Leaderboard</h4>
+          <Link to="/leaderboard">
+            <h4>View Leaderboard</h4>
+          </Link>
         </div>
       </main>
     </>
---
+++ src/components/ThemeContext.jsx
@@ -0,0 +1,23 @@
+// ThemeContext.js
+import React, { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("dark_theme");
+
+  const toggleTheme = () => {
+    console.log(theme);
+    setTheme((prevTheme) => (prevTheme === "dark_theme" ? "light_theme" : "dark_theme"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
