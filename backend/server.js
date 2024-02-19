b4dae68676d2c7e09167a2cbfaf94e8a30088bb4
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2024-02-19 17:31:57 +0100

    Update backend for vercel release

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,4 @@ app.post('/leaderboard', verifyApiKey, async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app; 
