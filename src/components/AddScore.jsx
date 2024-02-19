fb4bcdd7b2ef7375458bac31460dea50d03e68a1
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2024-02-19 17:13:27 +0100

    Update styles for score form and fix theme icon rerendering

--- src/components/AddScore.jsx
+++ src/components/AddScore.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import oliverCityCard from '/levels/oliver-city/oliver-city-card.png';
+import prehistoriaCard from '/levels/prehistoria/prehistoria-card.png';
+import dragonIslandCard from '/levels/dragon-island/dragon-island-card.png';
 
 const AddScoreForm = ({ level, time }) => {
   const [formData, setFormData] = useState({
@@ -7,6 +11,7 @@ const AddScoreForm = ({ level, time }) => {
   });
   const [updatedLevel, setUpdatedLevel] = useState('');
   const [convertedTime, setConvertedTime] = useState(0);
+  const [cardImg, setCardImg] = useState(oliverCityCard);
 
   const navigateTo = useNavigate();
 
@@ -21,9 +26,11 @@ const AddScoreForm = ({ level, time }) => {
         break;
       case 'prehistoria':
         setUpdatedLevel('Prehistoria');
+        setCardImg(prehistoriaCard);
         break;
       case 'dragon':
         setUpdatedLevel(`Dragon Island`);
+        setCardImg(dragonIslandCard);
         break;
       default:
         setUpdatedLevel('');
@@ -69,34 +76,47 @@ const AddScoreForm = ({ level, time }) => {
   };
 
   return (
-    <div className="score_form">
-      <div className="score_form__content">
-        <h2>{updatedLevel}</h2>
-        <h3 className="score_form__content__text">
-          Time: {convertedTime} minutes
-        </h3>
-        <p>Add your score to the leaderboard</p>
-        <form className="score_form__content__inputs" onSubmit={handleSubmit}>
-          <input
-            className="form_name_input"
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Your nickname..."
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-          <button className="form_submit_btn" type="submit">
-            Submit
-          </button>
-        </form>
-        <Link className="score_form__content__return" to="/">
-          <button className="score_form__content__return_btn">
-            Back to Main Menu
-          </button>
-        </Link>
-      </div>
-    </div>
+    <AnimatePresence> 
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+           <div className="score_form">
+            <div className="score_form__content">
+              <img className="score_form__img" src={cardImg} alt="level-picture" />
+              <div className="score_form__content__text">
+                <h2 className="score_form__content__text_title">{updatedLevel}</h2>
+                <h3 className="score_form__content__text_time">{convertedTime} minutes</h3>
+              </div>
+              <p>Add your score to the leaderboard</p>
+              <form className="score_form__content__inputs" onSubmit={handleSubmit}>
+                <input
+                  className="form_name_input"
+                  type="text"
+                  id="name"
+                  name="name"
+                  minlength="2"
+                  maxlength="12"
+                  placeholder="Your nickname..."
+                  required
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+                <button className="form_submit_btn" type="submit">
+                  Submit
+                </button>
+              </form>
+              <Link className="score_form__content__return" to="/">
+                <button className="score_form__content__return_btn">
+                  Back to Main Menu
+                </button>
+              </Link>
+            </div>
+          </div>
+        </motion.div>
+    </AnimatePresence>
   );
 };
 
